9e99060e2763aad2a2c7e896d2dde483147d874e
Venkata Adithya Boppana <bv.adithya369@gmail.com>
2024-02-19 15:05:51 -0600

    optimized version

--- arrays/containsDuplicates.py
+++ arrays/containsDuplicates.py
@@ -1,4 +1,3 @@
-# This can also be done using sorting. which might have a complexity less than o(n)
 def containsDuplicate(nums: list[int]) -> bool:
     returnBool = True;
     dictNums = {}
@@ -12,5 +11,14 @@ def containsDuplicate(nums: list[int]) -> bool:
             returnBool = False
     return returnBool
 
+def containsDuplicateOptimized(nums: list[int]) -> bool:
+    dictNums = {}
+    for num in nums:
+         if num in dictNums:
+             return True
+         else:
+            dictNums[num]=1
+    return False
+
 if __name__ == "__main__":
-    print(containsDuplicate([1,2,3,4,5,1]))
+    print(containsDuplicateOptimized([1,2,3,4,5,1]))
